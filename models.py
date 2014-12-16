019fa463ba4e9107d30d866c53e6a968d10024f4
Kai Diefenbach <kai.diefenbach@iqpp.de>
2014-12-16 23:55:39 +0100

    Fixes models fields.

--- models.py
+++ models.py
@@ -3,8 +3,8 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Url(models.Model):
-    url = models.TextField(_(u"Url"), null=True)
-    short_url = models.CharField(_(u"Short URL"), max_length=50, null=True, unique=True)
+    url = models.TextField(_(u"Url"))
+    short_url = models.CharField(_(u"Short URL"), max_length=50, unique=True)
 
     def __unicode__(self):
         return "{} -> {}".format(self.short_url, self.url)
